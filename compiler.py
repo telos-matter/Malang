c669dc6a4a7e2932bef1cfb613dbc111c54daac2
HEMMOUDA Aymane <ahemmouda@gmail.com>
2023-10-23 09:25:04 +0100

    Better error reporting

--- compiler.py
+++ compiler.py
@@ -11,15 +11,23 @@ class TokenType (Enum):
     ASSIGN_OP   = auto()     # The assigning operation, `=`
 
 class Token ():
-    def __init__(self, tokenType: TokenType, lexeme: str | float, file_path: str, line_index: int, char_index: int) -> None:
+    def __init__(self, tokenType: TokenType, lexeme: str | float | int, line: str, span: int, file_path: str, line_index: int, char_index: int) -> None:
+        '''`line`: the line in which this Token exists\n
+        `span`: the length of the Token in the line
+        '''
         self.type = tokenType
         self.lexeme = lexeme
+        self.line = line
+        self.span = span
         self.file = file_path
-        self.line = line_index +1
-        self.char = char_index +1
+        self.line_number = line_index +1
+        self.char_number = char_index +1
     
     def location(self) -> str:
-        return f"File `{self.file}`, line: {self.line}, column: {self.char}"
+        return f"File `{self.file}`, line: {self.line_number}, column: {self.char_number}"
+    
+    def pointOut(self) -> str:
+        return f"{self.line[:self.char_number -1]}>>>{self.line[self.char_number -1 : self.char_number -1 +self.span]}<<<{self.line[self.char_number -1 +self.span:]}"
     
     def __repr__(self) -> str:
         return str(self.lexeme)
@@ -28,6 +36,11 @@ def parseSourceFile (content: str, filePath: str) -> list[Token]:
     '''Takes a source file and parses its content to tokens
     Does not check for structure validity,
     only checks for content correctness'''
+    
+    def parsingError (message: str, temp_token: Token) -> None:
+        '''Raises a parsing error exception'''
+        message = "PARSING ERROR: " +message +f"\n{temp_token.pointOut()}\n{temp_token.location()}"
+        raise Exception(message)
     
     NUMBER_PERIOD = '.'
     tokens = []
@@ -40,7 +53,7 @@ def parseSourceFile (content: str, filePath: str) -> list[Token]:
             if char in OP_SET.getSymbols():
                 if char == OP_SET.OP_IDIV.symbol[0] and i +1 < len(line) and line[i +1] == OP_SET.OP_IDIV.symbol[1]:
                     char = line[i : i +2]
-                tokens.append(Token(TokenType.OP, char, filePath, line_index, i))
+                tokens.append(Token(TokenType.OP, char,line, len(char), filePath, line_index, i))
                 i += len(char)
             
             elif char.isdigit():
@@ -51,17 +64,20 @@ def parseSourceFile (content: str, filePath: str) -> list[Token]:
                     j += 1
                 try:
                     number = float(number)
+                    if int(number) == number:
+                        number = int(number)
                 except ValueError:
-                    raise Exception(f"PARSING ERROR: Couldn't parse this number: `{number}`\nFile `{filePath}`, line: {line_index +1}, column: {i +1}")
-                tokens.append(Token(TokenType.NUMBER, number, filePath, line_index, i))
+                    temp_token = Token(None, number, line, j -i, filePath, line_index, i)
+                    parsingError(f"Couldn't parse this number: `{number}`", temp_token)
+                tokens.append(Token(TokenType.NUMBER, number, line, j -i, filePath, line_index, i))
                 i = j
             
             elif char == '(':
-                tokens.append(Token(TokenType.OPEN_PAREN, char, filePath, line_index, i))
+                tokens.append(Token(TokenType.OPEN_PAREN, char, line, len(char), filePath, line_index, i))
                 i += 1
             
             elif char == ')':
-                tokens.append(Token(TokenType.CLOSE_PAREN, char, filePath, line_index, i))
+                tokens.append(Token(TokenType.CLOSE_PAREN, char, line, len(char), filePath, line_index, i))
                 i += 1
             
             elif char == '_' or char.isalpha():
@@ -70,24 +86,25 @@ def parseSourceFile (content: str, filePath: str) -> list[Token]:
                 while j < len(line) and (line[j].isalpha() or line[j].isdigit() or line[j] == '_'):
                     identifier += line[j]
                     j += 1
-                tokens.append(Token(TokenType.IDENTIFIER, identifier, filePath, line_index, i))
+                tokens.append(Token(TokenType.IDENTIFIER, identifier, line, j -i, filePath, line_index, i))
                 i = j
             
             elif char == '=':
-                tokens.append(Token(TokenType.ASSIGN_OP, char, filePath, line_index, i))
+                tokens.append(Token(TokenType.ASSIGN_OP, char, line, len(char), filePath, line_index, i))
                 i += 1
             
             elif char == '#':
-                # A line comment, go to next line
+                # A line comment, go to the next line
                 break
             
             elif char.isspace():
                 i += 1
             
             else:
-                raise Exception(f"PARSING ERROR: Unexpected char: `{char}`\nFile `{filePath}`, line: {line_index +1}, column: {i +1}")
+                temp_token = Token(None, char, line, len(char), filePath, line_index, i)
+                parsingError(f"Unexpected char: `{char}`", temp_token)
         
-        tokens.append(Token(TokenType.EOL, '\n', filePath, line_index, len(line))) # Fuck Windows' new lines
+        tokens.append(Token(TokenType.EOL, '\n', line, 1, filePath, line_index, len(line))) # Fuck Windows' new lines
     
     return tokens
 
@@ -124,6 +141,11 @@ def constructAST (tokens: list[Token]) -> list[Node]:
     Does not check for the validity of the
     code (referencing a none existing variable for example or recursion),
     only checks for validity of the structure / syntax'''
+    
+    def syntaxError (message: str, token: Token) -> None:
+        '''Raises a syntax error exception'''
+        message = "SYNTAX ERROR: " +message +f"\n{token.pointOut()}\n{token.location()}"
+        raise Exception(message)
     
     def producesValue (element: Token | Node) -> bool:
         '''Checks if the element is a value element'''
@@ -197,22 +219,22 @@ def constructAST (tokens: list[Token]) -> list[Node]:
             
             if tokenType == TokenType.NUMBER:
                 if len(buffer) != 0:
-                    raise Exception(f"SYNTAX ERROR: Was not expecting a number here.\n{token.location()}")
+                    syntaxError(f"The number `{token}` can't be here! Expected an operation.", token)
                 buffer.append(token)
                 i += 1
             
             elif tokenType == TokenType.IDENTIFIER:
                 if len(buffer) != 0:
-                    raise Exception(f"SYNTAX ERROR: Was not expecting an identifier here.\n{token.location()}")
+                    syntaxError(f"The variable `{token}` can't be here! Expected an operation", token)
                 buffer.append(token)
                 i += 1
             
             elif tokenType == TokenType.OPEN_PAREN:
                 if len(buffer) != 0:
-                    raise Exception(f"SYNTAX ERROR: Was not expecting an open parenthesis here.\n{token.location()}")
+                    syntaxError(f"This open parenthesis `{token}` can't be here! Expected an operation", token)
                 close_paren = findEnclosingToken(tokens, TokenType.OPEN_PAREN, TokenType.CLOSE_PAREN, i +1)
                 if close_paren == None:
-                    raise Exception(f"SYNTAX ERROR: The closing parenthesis is missing.\n{token.location()}")
+                    syntaxError(f"The closing parenthesis for this one is missing!", token)
                 value = processValueExpression(tokens[i +1 : close_paren], token)
                 buffer.append(Node(NodeType.ORDER_PAREN, value=value))
                 i = close_paren +1
@@ -220,14 +242,14 @@ def constructAST (tokens: list[Token]) -> list[Node]:
             elif tokenType == TokenType.OP:
                 if len(buffer) != 1 or not producesValue(buffer[0]): # Should have already found a value before
                     if len(buffer) == 0:
-                        raise Exception(f"SYNTAX ERROR: This operation `{token}` requires a left-hand side value.\n{token.location()}")
+                        syntaxError(f"There should be a left-hand side value for this operation `{token}`", token)
                     elif len(buffer) == 1:
                         assert False, f"Buffer has a none value element: {buffer[0]}"
                         # raise Exception(f"SYNTAX ERROR: Can't use this `{buffer[0]}` with this operation `{tokens[i]}`.\n{tokens[i].location()}")
                     else:
                         assert False, f"Buffer has more than one element: {buffer}"
                 if len(tokens) <= i +1:
-                    raise Exception(f"SYNTAX ERROR: Expected something after this operation `{token}`.\n{token.location()}")
+                    syntaxError(f"There should be a right-hand side value for this operation `{token}`", token)
                 r_value = None
                 if tokens[i +1].type in [TokenType.NUMBER, TokenType.IDENTIFIER]: # TODO separate once we have func_call
                     r_value = tokens[i +1]
@@ -235,12 +257,12 @@ def constructAST (tokens: list[Token]) -> list[Node]:
                 elif tokens[i +1].type == TokenType.OPEN_PAREN:
                     close_paren = findEnclosingToken(tokens, TokenType.OPEN_PAREN, TokenType.CLOSE_PAREN, i +2)
                     if close_paren == None:
-                        raise Exception(f"SYNTAX ERROR: The closing parenthesis is missing.\n{token.location()}")
+                        syntaxError(f"The closing parenthesis for this one is missing!", tokens[i +1])
                     value = processValueExpression(tokens[i +2 : close_paren], tokens[i +1])
                     r_value = Node(NodeType.ORDER_PAREN, value=value)
                     i = close_paren +1
                 else:
-                    raise Exception(f"SYNTAX ERROR: Was not expecting this after this operation `{token}`.\n{token.location()}")
+                    syntaxError(f"This operation `{token}` requires a value after it, not this `{tokens[i +1]}`", tokens[i +1])
                 if type(buffer[0]) == Node and buffer[0].type == NodeType.OP: # If an op is the previous value then append
                     buffer[0] = appendOP(buffer[0], token, r_value)
                 else: # Otherwise create one
@@ -250,10 +272,10 @@ def constructAST (tokens: list[Token]) -> list[Node]:
                 i += 1
             
             else:
-                raise Exception(f"SYNTAX ERROR: Was not expecting this `{tokens[i]}` here.\n{tokens[i].location()}")
+                syntaxError(f"What is this `{token}` doing here? It shouldn't be there", token)
         if len(buffer) != 1 or not producesValue(buffer[0]):
             if len(buffer) == 0:
-                raise Exception(f"SYNTAX ERROR: Expected something after this `{parent_token}`.\n{parent_token.location()}")
+                syntaxError(f"Expected some sort of expression after this", parent_token)
             elif len(buffer) == 1:
                 assert False, f"Buffer has a none value element: {buffer[0]}"
                 # raise Exception(f"SYNTAX ERROR: Can't assign this `{buffer[0]}` to this variable `{token}`.\n{token.location()}")
@@ -269,7 +291,7 @@ def constructAST (tokens: list[Token]) -> list[Node]:
         
         if tokenType == TokenType.IDENTIFIER: # var_assign
             if len(tokens) <= i +1 or tokens[i +1].type != TokenType.ASSIGN_OP:
-                raise Exception(f"SYNTAX ERROR: Expected the assign operation `=` after this variable `{token}`.\nVariables can't be declared without assigning a value to then.\n{token.location()}")
+                syntaxError(f"There should be an assignment operator after this variable `{token}`\nVariables can't be declared without assigning a value to them", token)
             i += 2
             eol = findToken(tokens, TokenType.EOL, i)
             assert eol != None, f"EOL was not appended to the line of this token: {token}\n{token.location()}"
@@ -277,7 +299,7 @@ def constructAST (tokens: list[Token]) -> list[Node]:
             if open_paren != None: # If there is a paren, then look for the last EOL
                 end_paren = findEnclosingToken(tokens, TokenType.OPEN_PAREN, TokenType.CLOSE_PAREN, open_paren +1)
                 if end_paren == None:
-                    raise Exception(f"SYNTAX ERROR: The closing parenthesis is missing.\n{tokens[open_paren].location()}")
+                    syntaxError(f"The closing parenthesis for this one is missing!", tokens[open_paren])
                 eol = findToken(tokens, TokenType.EOL, end_paren +1)
                 assert eol != None, f"EOL was not appended to the line of this token: {token}\n{token.location()}"
             ast.append(Node(NodeType.VAR_ASSIGN, var=token, value=processValueExpression(tokens[i : eol], tokens[i -1])))
@@ -285,10 +307,9 @@ def constructAST (tokens: list[Token]) -> list[Node]:
         
         elif tokenType == TokenType.EOL: # Skip
             i += 1
-        # TODO add support in processValue for open paren
         
         else:
-            raise Exception(f"This `{token}` shouldn't be here here!\n{token.location()}")
+            syntaxError(f"This `{token}` cannot start a new expression", token)
     
     return ast
 
